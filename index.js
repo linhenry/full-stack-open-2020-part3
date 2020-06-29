55e1a77048d216b8c73526c27b5e4ca23070731c
Henry Lin <henry.lin.hln@gmail.com>
2020-06-28 20:16:14 -0400

    Exercise 3.3 completed
    Added requests folder for VSCode REST Client and functionality for retrieving a single person

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { response } = require('express')
 const app = express()
 
 app.use(express.json())
@@ -32,6 +33,17 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.get('/info', (req, res) => {
