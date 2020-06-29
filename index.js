773db76bb25b213e6a313c8143ec46d5b7c82b59
Henry Lin <henry.lin.hln@gmail.com>
2020-06-29 19:38:13 -0400

    Exercise 3.7 completed
    Added morgan middleware (tiny configuration) for logging

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -48,7 +50,6 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  console.log(persons)
   res.status(204).end()
 })
 
